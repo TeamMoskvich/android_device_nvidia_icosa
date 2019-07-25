8b4d15b7e43af7acc20d76944ffa58df133c4eb0
Billy Laws <blaws05@gmail.com>
2019-07-25 17:11:11 +0100

    Add sdcard support

--- twrp/twrp.mk
+++ twrp/twrp.mk
@@ -1,10 +1,11 @@
 PRODUCT_COPY_FILES += \
                       device/nvidia/foster/twrp/twrp.fstab.emmc:recovery/root/etc/twrp.fstab.emmc \
+                      device/nvidia/foster/twrp/twrp.fstab.sdcard:recovery/root/etc/twrp.fstab.sdcard \
                       device/nvidia/foster/twrp/twrp.fstab.sata:recovery/root/etc/twrp.fstab.sata
 
 TW_THEME := landscape_hdpi
 TW_HWROTATION := 270
 TW_NO_SCREEN_TIMEOUT := true
-TW_NO_SCREEN_BLANK := true
+TW_NO_SCREEN_BLANK := false
 TW_BRIGHTNESS_PATH := /sys/class/backlight/pwm-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
